9f063105931ff8cec4bcb4ed9922b474d7a99d27
Abhishek Kaushik <akaushik079@gmail.com>
2024-03-19 21:46:31 +0530

    Added program to print N to 1 recursively

---
+++ src/recursion/PrintNto1.java
@@ -0,0 +1,14 @@
+package recursion;
+
+public class PrintNto1 {
+    public static void main(String[] args) {
+        printNto1(5);
+    }
+
+    static void printNto1(int n) {
+        if (n <= 0)
+            return;
+        System.out.println(n);
+        printNto1(n - 1);
+    }
+}
