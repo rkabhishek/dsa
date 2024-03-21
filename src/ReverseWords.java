8a62ffb5432841f7607bb65c264f11ee17a396e9
Abhishek Kaushik <akaushik079@gmail.com>
2024-03-21 22:12:11 +0530

    Added solution to Reverse Each Word in a string

---
+++ src/ReverseWords.java
@@ -0,0 +1,33 @@
+// leetcode 557
+
+public class ReverseWords {
+    public String reverseWords(String s) {
+        char[] sentence = s.toCharArray();
+
+        int wordStart = 0;
+        int spaceIndex = 0;
+
+        while (wordStart < sentence.length) {
+
+            while (spaceIndex < sentence.length && sentence[spaceIndex] != ' ')
+                spaceIndex++;
+
+            reverse(sentence, wordStart, spaceIndex - 1);
+            wordStart = spaceIndex + 1;
+            spaceIndex = wordStart;
+        }
+
+        return new String(sentence);
+    }
+
+    private void reverse(char[] s, int start, int end) {
+        char temp;
+        while (start < end) {
+            temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
