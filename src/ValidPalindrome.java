150ca69b35a98da995210802ef06f3a2e450d4e0
Abhishek Kaushik <akaushik079@gmail.com>
2024-03-21 12:35:12 +0530

    Added solution for checking if string is a valid palindrome

---
+++ src/ValidPalindrome.java
@@ -0,0 +1,20 @@
+// leetcode 125
+
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        char start;
+        char end;
+        int len = s.length();
+
+        for (int i = 0; i < len / 2; i++) {
+            start = s.charAt(i);
+            end = s.charAt(len - i - 1);
+            if (start != end)
+                return false;
+        }
+
+        return true;
+    }
+}
