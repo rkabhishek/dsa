d8437616cd213100048f1d13f8386f0a952204b2
Abhishek Kaushik <akaushik079@gmail.com>
2024-03-21 23:04:50 +0530

    Added solution to reverse order of words in a character array

---
+++ src/ReverseOrderOfWords2.java
@@ -0,0 +1,34 @@
+// leetcode 186
+
+public class ReverseOrderOfWords2 {
+    public void reverseWords(char[] s) {
+        reverseString(s, 0, s.length - 1);
+        reverseEachWord(s);
+    }
+
+    private void reverseEachWord(char[] s) {
+        int wordStart = 0;
+        int spaceIndex = 0;
+
+        while (wordStart < s.length) {
+            while (spaceIndex < s.length && s[spaceIndex] != ' ')
+                spaceIndex++;
+
+            reverseString(s, wordStart, spaceIndex - 1);
+            wordStart = spaceIndex + 1;
+            spaceIndex = wordStart;
+        }
+
+    }
+
+    private void reverseString(char[] s, int start, int end) {
+        char temp;
+        while (start < end) {
+            temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
