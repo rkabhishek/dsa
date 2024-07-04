b3d45b036abfb2d4e3e2895ea8fd7f888d6e2ecb
Abhishek Kaushik <akaushik079@gmail.com>
2024-07-04 13:03:21 +0530

    Add solution for Build Permutation Array that doesn't use extra space

--- src/arrays/BuildPermutationArray.java
+++ src/arrays/BuildPermutationArray.java
@@ -1,12 +1,17 @@
 package arrays;
 
+// leetcode 1920
 public class BuildPermutationArray {
     public int[] buildArray(int[] nums) {
-        int[] ans = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            ans[i] = nums[nums[i]];
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int newValue = (nums[nums[i]] % n);
+            nums[i] = nums[i] + newValue * n;
+        }
+        for (int i = 0; i < n; i++) {
+            nums[i] /= n;
         }
         
-        return ans;
+        return nums;
     }
 }
