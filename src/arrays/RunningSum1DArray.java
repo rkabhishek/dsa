9f75fcfd15919bad72ae16623a11c451e190d2b1
Abhishek Kaushik <akaushik079@gmail.com>
2024-07-04 16:05:57 +0530

    Add solution for Running sum of 1D array

---
+++ src/arrays/RunningSum1DArray.java
@@ -0,0 +1,14 @@
+package arrays;
+
+// leetcode 1480
+public class RunningSum1DArray {
+    public int[] runningSum(int[] nums) {
+        int[] sum = new int[nums.length];
+        sum[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum[i] = sum[i - 1] + nums[i];
+        }
+
+        return sum;
+    }
+}
