6868549cc7090b9c4e4ed70a8d6b64c249323e5f
Abhishek Kaushik <akaushik079@gmail.com>
2024-07-05 13:38:21 +0530

    Add Hashmap solution for Good pairs

--- src/arrays/GoodPairs.java
+++ src/arrays/GoodPairs.java
@@ -1,15 +1,21 @@
 package arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // leetcode 1512
 public class GoodPairs {
     public int numIdenticalPairs(int[] nums) {
         int count = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    count++;
-                }
-            }
+        Map<Integer, Integer> counts = new HashMap<>();
+
+        for (int num: nums) {
+            int numCount = counts.getOrDefault(num, 0);
+            counts.put(num, numCount + 1);
+        }
+
+        for (Integer c: counts.values()) {
+            count += (c * (c - 1)) / 2;
         }
 
         return count;
