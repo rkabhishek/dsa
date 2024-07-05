e77345717b55f4eb77f9aa66b94249ce6c69182c
Abhishek Kaushik <akaushik079@gmail.com>
2024-07-05 13:51:21 +0530

    Modify Good Pairs solution to count pairs while updating the hashmap

--- src/arrays/GoodPairs.java
+++ src/arrays/GoodPairs.java
@@ -6,18 +6,15 @@ import java.util.Map;
 // leetcode 1512
 public class GoodPairs {
     public int numIdenticalPairs(int[] nums) {
-        int count = 0;
+        int identicalPairCount = 0;
         Map<Integer, Integer> counts = new HashMap<>();
 
         for (int num: nums) {
             int numCount = counts.getOrDefault(num, 0);
+            identicalPairCount += numCount;
             counts.put(num, numCount + 1);
         }
 
-        for (Integer c: counts.values()) {
-            count += (c * (c - 1)) / 2;
-        }
-
-        return count;
+        return identicalPairCount;
     }
 }
