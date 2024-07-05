28cdb3a8ff1be8084aeedc0d6132942a5ddc9df1
Abhishek Kaushik <akaushik079@gmail.com>
2024-07-05 12:15:52 +0530

    Improve Single Row Keyboard solution to use constant time lookup for indices in keyboard

--- src/hashing/SingleRowKeyboard.java
+++ src/hashing/SingleRowKeyboard.java
@@ -1,14 +1,20 @@
 package hashing;
 
+// leetcode 1165
 public class SingleRowKeyboard {
     public int calculateTime(String keyboard, String word) {
+        int[] keyboardIndices = new int[26];
+        for (int i = 0; i < keyboard.length(); i++) {
+            keyboardIndices[keyboard.charAt(i) - 'a'] = i;
+        }
+
         int typingTime = 0;
         int prevIndex = 0;
 
         for (char c: word.toCharArray()) {
-            int currentIndex = keyboard.indexOf(c);
-            typingTime = typingTime + Math.abs(currentIndex - prevIndex);
-            prevIndex = currentIndex;
+            int currIndex = keyboardIndices[c -'a'];
+            typingTime += Math.abs(currIndex - prevIndex);
+            prevIndex = currIndex;
         }
 
         return typingTime;
