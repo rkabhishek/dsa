85ed57c73cdb78a25d7409480713ff87c9c93c80
Abhishek Kaushik <akaushik079@gmail.com>
2024-09-01 01:28:52 +0530

    Add code for Armstrong Number

---
+++ src/main/java/leetcode/ArmstrongNumber.java
@@ -0,0 +1,20 @@
+package main.java.leetcode;
+
+public class ArmstrongNumber {
+    public boolean isArmstrong(int n) {
+        int digitCount = String.valueOf(n).length();
+
+        return getSumOfKthPowerOfDigits(n, digitCount) == n;
+    }
+
+    private int getSumOfKthPowerOfDigits(int n, int digitCount) {
+        int sum = 0;
+
+        while (n > 0) {
+            sum += Math.pow(n % 10, digitCount);
+            n /= 10;
+        }
+
+        return sum;
+    }
+}
