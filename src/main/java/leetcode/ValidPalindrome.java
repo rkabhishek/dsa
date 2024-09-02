47041430543de44bf8709beb217ede8abfdf44e4
Abhishek Kaushik <akaushik079@gmail.com>
2024-09-02 12:44:53 +0530

    Add solution for Valid Palindrome

---
+++ src/main/java/leetcode/ValidPalindrome.java
@@ -0,0 +1,21 @@
+package main.java.leetcode;
+
+// leetcode 125
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
