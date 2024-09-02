bec3eccb67143684900d26416ac8316807fbc209
Abhishek Kaushik <akaushik079@gmail.com>
2024-09-02 10:57:30 +0530

    Replace checking loop control variable <= Math.sqrt with O(1) operation

--- src/main/java/practice/Prime.java
+++ src/main/java/practice/Prime.java
@@ -6,7 +6,7 @@ public class Prime {
             return false;
         }
 
-        for (int i = 2; i <= Math.sqrt(n); i++) {
+        for (int i = 2; i * i <= n; i++) {
             if (n % i == 0) {
                 return false;
             }
--- src/main/java/practice/PrintDivisors.java
+++ src/main/java/practice/PrintDivisors.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class PrintDivisors {
     public void printDivisors(int n) {
-        for (int i = 1; i <= Math.sqrt(n); i++) {
+        for (int i = 1; i * i <= n; i++) {
             if (n % i == 0) {
                 System.out.print(i + " ");
                 if (n / i != i) {
@@ -20,7 +20,7 @@ public class PrintDivisors {
     public void printDivisors2(int n) {
         List<Integer> divisors = new ArrayList<>();
 
-        for (int i = 1; i <= Math.sqrt(n); i++) {
+        for (int i = 1; i * i <= n; i++) {
             if (n % i == 0) {
                 divisors.add(i);
                 if (n / i != i) {
