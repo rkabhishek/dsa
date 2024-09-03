0d3c29f6837039e77cb38beb8baa47447bc6b059
Abhishek Kaushik <akaushik079@gmail.com>
2024-09-03 16:10:38 +0530

    Add solution for Build Array From Permutation

---
+++ src/main/java/leetcode/BuildArrayFromPermutation.java
@@ -0,0 +1,15 @@
+package main.java.leetcode;
+
+// leetcode 1920
+public class BuildArrayFromPermutation {
+
+    public int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+}
