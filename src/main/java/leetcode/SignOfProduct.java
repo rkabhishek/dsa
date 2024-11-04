6a74f2308ce941e07079ee175b1229bf35b7dcc8
Abhishek Kaushik <akaushik079@gmail.com>
2024-11-04 17:46:37 +0530

    Add solution for Sign of Product

---
+++ src/main/java/leetcode/SignOfProduct.java
@@ -0,0 +1,19 @@
+package main.java.leetcode;
+
+// leetcode 1822
+public class SignOfProduct {
+    public int arraySign(int[] nums) {
+        int negativeCount = 0;
+
+        for (int num: nums) {
+            if (num == 0) {
+                return 0;
+            }
+            else if (num < 0) {
+                negativeCount++;
+            }
+        }
+
+        return (negativeCount % 2 == 0) ? 1 : -1;
+    }
+}
