83c05f16bac7d906b8c51c7a3f357205beadc234
Abhishek Kaushik <akaushik079@gmail.com>
2024-11-04 14:56:46 +0530

    Improved solution for Find the Difference to not use arrays to store counts of characters; instead use sum of ascii values

--- src/main/java/leetcode/FindDifference.java
+++ src/main/java/leetcode/FindDifference.java
@@ -2,27 +2,18 @@ package main.java.leetcode;
 
 // leetcode 389
 public class FindDifference {
-    private static final int ALPHABET_SIZE = 26;
-    private static final int ASCII_A = 97;
-
     public char findTheDifference(String s, String t) {
-        int[] characterCountS = new int[ALPHABET_SIZE];
-        int[] characterCountT = new int[ALPHABET_SIZE];
+        int sumOfCharS = 0;
+        int sumOfCharT = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            characterCountS[s.charAt(i) - ASCII_A]++;
+            sumOfCharS += s.charAt(i);
         }
 
         for (int i = 0; i < t.length(); i++) {
-            characterCountT[t.charAt(i) - ASCII_A]++;
+            sumOfCharT += t.charAt(i);
         }
 
-        for (int i = 0; i < ALPHABET_SIZE; i++) {
-            if (characterCountS[i] != characterCountT[i]) {
-                return (char)(i + ASCII_A);
-            }
-        }
-
-        throw new IllegalArgumentException("Invalid input: The second string must contain one additional character compared to the first string");
+        return (char)(sumOfCharT - sumOfCharS);
     }
 }
