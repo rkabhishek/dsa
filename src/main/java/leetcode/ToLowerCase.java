3e9584041431e32fb182d2ec6dd69d1b9d9b0bfb
Abhishek Kaushik <akaushik079@gmail.com>
2024-11-05 13:48:45 +0530

    Update solution for To Lower Case to use StringBuilder instead of built-in method

--- src/main/java/leetcode/ToLowerCase.java
+++ src/main/java/leetcode/ToLowerCase.java
@@ -3,6 +3,10 @@ package main.java.leetcode;
 // leetcode 709
 public class ToLowerCase {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        StringBuilder sb = new StringBuilder();
+        for (char c: s.toCharArray()) {
+            sb.append(Character.toLowerCase(c));
+        }
+        return sb.toString();
     }
 }
