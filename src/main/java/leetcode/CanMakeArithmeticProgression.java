2420624aef7a326bdd806f14bd8c2b3bcee9ecbf
Abhishek Kaushik <akaushik079@gmail.com>
2024-11-09 00:40:56 +0530

    Add solution for Can Make Arithmetic Progression From Sequence which takes O(n) time and O(n) space

--- src/main/java/leetcode/CanMakeArithmeticProgression.java
+++ src/main/java/leetcode/CanMakeArithmeticProgression.java
@@ -1,6 +1,8 @@
 package main.java.leetcode;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 // leetcode 1502
 public class CanMakeArithmeticProgression {
@@ -10,6 +12,36 @@ public class CanMakeArithmeticProgression {
 
         for (int i = 1; i < arr.length - 1; i++) {
             if (arr[i + 1] - arr[i] != expectedDiff) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean canMakeArithmeticProgression2(int[] arr) {
+        Set<Integer> numSet = new HashSet<>();
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for (int num: arr) {
+            numSet.add(num);
+            if (num > max) {
+                max = num;
+            }
+            if (num < min) {
+                min = num;
+            }
+        }
+
+        if ((max - min) % (arr.length - 1) != 0) {
+            return false;
+        }
+        int possibleDifference = (max - min) / (arr.length - 1);
+
+        for (int i = 0; i <= arr.length - 1; i++) {
+            int num = min + i * possibleDifference;
+            if (!numSet.contains(num)) {
                 return false;
             }
         }
